9942c0e781b065682c88b9f61d488e558e878261
tungdo <sont4036@gmail.com>
2024-12-10 17:43:16 +0700

    push src code

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebBanLapTop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
