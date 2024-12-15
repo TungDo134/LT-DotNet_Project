d96718aee5ce661f6e11ff216036cd15be93de74
tungdo <sont4036@gmail.com>
2024-12-15 22:59:34 +0700

    chuyen hien thi san pham qua api

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Diagnostics;
 using WebBanLapTop.Data;
 using WebBanLapTop.Models;
@@ -10,28 +11,39 @@ namespace WebBanLapTop.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly LaptopShopContext db;
+        private readonly IHttpClientFactory _httpClientFactory;
 
 
-        public HomeController(ILogger<HomeController> logger, LaptopShopContext context)
+        public HomeController(ILogger<HomeController> logger, LaptopShopContext context, IHttpClientFactory httpClientFactory)
         {
             _logger = logger;
             db = context;
+            _httpClientFactory = httpClientFactory;
         }
 
-        public IActionResult Index()
+
+
+
+        public async Task<IActionResult> Index()
         {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync($"https://localhost:7258/api/ProductAPI");
 
-            var list_pro_home = db.ChiTietSanPhams.AsQueryable();
-
-            var list = list_pro_home.Select(p => new HomeVM
+            if (response.IsSuccessStatusCode)
             {
-                Id = p.MaSp,
-                Name = p.TenSp ?? "",
-                Img = p.HinhAnh ?? "",
-                Price = p.DonGia ?? 0
+                // Đọc nội dung JSON trả về dưới dạng chuỗi
+                var jsonResponse = await response.Content.ReadAsStringAsync();
 
-            }); 
-            return View(list);
+                // Chuyển đổi chuỗi JSON thành IEnumerable<HomeVM>
+                IEnumerable<HomeVM> products = JsonConvert.DeserializeObject<IEnumerable<HomeVM>>(jsonResponse);
+
+                return View(products); // Trả về View với danh sách sản phẩm
+            }
+            else
+            {
+
+                return View("Error", new { Message = "Product not found" }); // Xử lý lỗi nếu không tìm thấy sản phẩm
+            }
 
         }
 
